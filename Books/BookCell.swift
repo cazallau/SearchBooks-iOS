fbdf2739d245ac0494332f8239eba9efb20ef796
Antonio Jesus Cazalla <gemabeltran@MacBook-Air-de-Gema.local>
2017-03-30 19:21:12 +0200

    Added bookCell for model cell

---
+++ Books/BookCell.swift
@@ -0,0 +1,24 @@
+//
+//  BookCell.swift
+//  Books
+//
+//  Created by Gema Beltran on 30/3/17.
+//  Copyright © 2017 Antonio Jesus Cazalla. All rights reserved.
+//
+
+import UIKit
+
+class BookCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
