cbf6c21adeccf5ee2dc83013311adbfd82ddf592
Antonio Jesus Cazalla <gemabeltran@MacBook-Air-de-Gema.local>
2017-03-31 11:46:03 +0200

    Added class to download and display the images of internet

---
+++ Books/APIDownloadImage.swift
@@ -0,0 +1,32 @@
+//
+//  APIDownloadImage.swift
+//  Books
+//
+//  Created by Gema Beltran on 31/3/17.
+//  Copyright © 2017 Antonio Jesus Cazalla. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class APIDownloadImage {
+    static func downloadImage(_ uri: String?, inView: UIImageView) {
+        guard let uri = uri, let url = URL(string: uri) else {
+            print("😱 Error in url")
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { responseData, response, error in
+            guard error == nil, let data = responseData else {
+                print("Error")
+                return
+            }
+            
+            DispatchQueue.main.async {
+                inView.image = UIImage(data: data)
+            }
+        }
+        
+        task.resume()
+    }
+}
